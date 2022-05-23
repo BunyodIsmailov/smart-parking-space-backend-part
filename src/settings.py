10519aad17d476ec18c18e9ff0917a5b367b968f
BunyodIsmailov <ishonch055@mail.ru>
2022-05-23 20:22:22 +0500

    model o'zgarish

--- src/settings.py
+++ src/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)&3ip_-aut8t%en7(5$m#o&k0j3qs@z-^avph4)rx^4#hcyw#_'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
